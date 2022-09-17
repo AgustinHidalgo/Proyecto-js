007b7e61cc0659510e379046c90edd8285faf557
agustinhidalgo10 <agustinhidalgocef18@gmail.com>
2022-09-16 21:37:29 -0300

    pruebas

---
+++ JS/index-2.js
@@ -0,0 +1,37 @@
+// aqui realizaremos el estudio y las pruebas de los conceptos
+
+//ciclos y bucles
+//empezamos con el for -> su estructura es for (desde, hasta, actualizacion) --> por lo general se usan numeros
+
+
+let numero1 = parseInt(prompt("Ingrese un numero del 1 al 10:"))
+let suma = 0
+
+for( let i = 0; i<10; i= i + 1){
+    let numero2= parseInt(prompt("Ingrese el numero que desea sumar:"))
+    suma = numero1 + numero2
+    console.log(suma)
+    
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// despues tenemos el while que sirve mas para bucles que no son numeros (aunque tambien se los puede usar con numeros) 
--- JS/index.js
+++ JS/index.js
@@ -30,36 +30,3 @@ console.log ("Este es el precio de la" + espaciado + productoA)
 
 
 
-//let nombre = "Homero"
-//let apellido = "Simpsons"
-//let edad = 39 
-
-//console.log("Este es mi nombre"+ espaciado + nombre + ",")
-//console.log("este mi apellido" + espaciado + apellido)
-//console.log("Y esta mi edad" + espaciado + edad)
-
-//const ciudad1 = "Springfield"
-//const ciudad2 = "Shelbyville"
-//const ciudad3 = "Capital City"
-//const ciudad4 = "Cypress Creek"
-//const ciudad5 = "Ogdenville"
-
-//console.log(ciudad1 + espaciado + "esta es la ciudad de" + espaciado + nombre)
-//console.log (ciudad2)
-//console.log (ciudad3)
-//console.log(ciudad4)
-//console.log(ciudad5)
-
-
-//let persona    = "BART SIMPSON"
-//let domicilio  = "742 EVERGREEN"
-//let pais       = "USA"
-//let nacimiento = "02-11-70"
-//const codigo   = "B47U89RE243"
-//let carnet ="Codigo:" + espaciado + codigo + espaciado +
-            //"Nombre:" + espaciado + persona + espaciado +
-            //"Pais:" + espaciado + pais + espaciado +
-            //"Nacimiento:" + espaciado + nacimiento + espaciado +
-            //"Domicilio:" + espaciado + domicilio; 
-
-//alert(carnet)
